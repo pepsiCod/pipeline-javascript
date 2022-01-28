8d9f086e0f0a160ca33d901457770366cfd8743e
Milos Petrovic <petrovic.milos.10@gmail.com>
2022-01-28 10:31:20 +0100

    commit

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Node.js Milos Petrovic i Uros Lopicic and Klevi Merkuri!\n');
+  res.send('Hello Node.js Milos Petrovic!\n');
 });
 
 var port = process.env.PORT||PORT;
